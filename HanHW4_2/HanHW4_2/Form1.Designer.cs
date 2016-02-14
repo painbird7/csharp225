5c41c2168afa6337b3726c788c6ac77ea8923c81
JAY HAN <djay.alpha@gmail.com>
2016-02-14 13:30:17 -0700

    Final

---
+++ HanHW4_2/HanHW4_2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace HanHW4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.desiredNumberTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many numbers would you like to generate?";
+            // 
+            // desiredNumberTextBox
+            // 
+            this.desiredNumberTextBox.Location = new System.Drawing.Point(326, 25);
+            this.desiredNumberTextBox.Name = "desiredNumberTextBox";
+            this.desiredNumberTextBox.Size = new System.Drawing.Size(100, 25);
+            this.desiredNumberTextBox.TabIndex = 1;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(336, 112);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(204, 112);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clea&r";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(67, 112);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "&Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 164);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.desiredNumberTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Number File Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox desiredNumberTextBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ HanHW4_2/HanHW4_2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*  Title: HanHW4_2 - Random Number File Writer
+    Requirement: Create an application that writes a series of random numbers to a file.  Each random number should be in the range of 1 through 100.  
+    The application should let the user specify how many random numbers the file will hold and should use a SaveFileDialog control to let the user 
+    specify the file's name and location.
+    Date: 02/14/2016   
+*/
+
+namespace HanHW4_2
+{
+    public partial class Form1 : Form
+    {
+        // Declare variables and create a list to store random numbers
+        Random rand;
+        int number;
+        int desiredNumber;
+        List<int> iList = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+        
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Parse user input and store it into desireNumber variable
+                desiredNumber = int.Parse(desiredNumberTextBox.Text);
+                // Generate a random number till the number user entered is down to 0 and add it to the iList
+                for (int n = desiredNumber; n > 0; n--)
+                {
+                    number = rand.Next(1, 101);
+                    iList.Add(number);
+                }
+
+                // Detect the path of documents folder and assign it to initial directory path
+                String myDocument = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                saveFile.InitialDirectory = myDocument;
+
+                // SaveFileDialog's save button click event
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // Initialize streamwriter
+                        StreamWriter writer = new StreamWriter(saveFile.OpenFile());
+                        // Iterate through and write each item in iList to file
+                        foreach (int i in iList)
+                        {
+                            writer.WriteLine(i);
+                        }
+                        // Close file
+                        writer.Close();
+                        MessageBox.Show("File has been saved.");
+                        desiredNumberTextBox.Text = string.Empty;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            desiredNumberTextBox.Text = string.Empty;
+        }
+    }
+}
