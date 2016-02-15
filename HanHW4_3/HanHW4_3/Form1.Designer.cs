62d7faeb4af4e24a40e1fac237e439a88e20afbd
JAY HAN <djay.alpha@gmail.com>
2016-02-14 19:35:10 -0700

    Final

---
+++ HanHW4_3/HanHW4_3/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace HanHW4_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.displayListBox = new System.Windows.Forms.ListBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // displayListBox
+            // 
+            this.displayListBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayListBox.FormattingEnabled = true;
+            this.displayListBox.ItemHeight = 15;
+            this.displayListBox.Location = new System.Drawing.Point(12, 13);
+            this.displayListBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.displayListBox.Name = "displayListBox";
+            this.displayListBox.Size = new System.Drawing.Size(87, 199);
+            this.displayListBox.TabIndex = 0;
+            this.displayListBox.TabStop = false;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 229);
+            this.openButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(87, 30);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "&Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(182, 229);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(87, 30);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clea&r";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(346, 229);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(87, 30);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "The total of the numbers:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(114, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "The number of random numbers read from the file:";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalTextBox.Location = new System.Drawing.Point(293, 33);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.ReadOnly = true;
+            this.totalTextBox.Size = new System.Drawing.Size(140, 23);
+            this.totalTextBox.TabIndex = 6;
+            this.totalTextBox.TabStop = false;
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countTextBox.Location = new System.Drawing.Point(293, 161);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.ReadOnly = true;
+            this.countTextBox.Size = new System.Drawing.Size(140, 23);
+            this.countTextBox.TabIndex = 7;
+            this.countTextBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 278);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.displayListBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Number File Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ListBox displayListBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.TextBox countTextBox;
+    }
+}
+
---
+++ HanHW4_3/HanHW4_3/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*  Title: HanHW4_3 - Random Number File Reader
+    Requirement: Create another application that uses an OpenFileDialog control to let the user select the file that was created by the 
+    Random Number File Writer application you wrote for problem #2 above.  This application should read the numbers from the file, display 
+    the numbers in a ListBox control, and then display the following data:
+    - The total of the numbers
+    - The number of random numbers read from the file
+    Date: 02/14/2016   
+*/
+
+namespace HanHW4_3
+{
+    public partial class Form1 : Form
+    {
+        // Create a list
+        List<int> iList = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Detect the path of documents folder and assign it to initial directory path
+                String myDocument = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFile.InitialDirectory = myDocument;
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // Initialize the streamreader
+                    StreamReader reader = new StreamReader(openFile.FileName);
+                    // Declare variable to store value in the file and iterate each line in the file and add to list
+                    string n;
+                    while ((n = reader.ReadLine()) != null)
+                    {
+                        iList.Add(int.Parse(n));
+                    }
+                    reader.Close();
+                    // Iterate through each item in iList and add to listbox control
+                    foreach (int i in iList)
+                    {
+                        displayListBox.Items.Add(i);
+                    }
+                    // Display sum and total count of numbers in the file
+                    countTextBox.Text = iList.Count.ToString();
+                    totalTextBox.Text = (iList.Sum()).ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            totalTextBox.Text = string.Empty;
+            countTextBox.Text = string.Empty;
+            displayListBox.Items.Clear();
+        }
+    }
+}
