a060ae64383883c3557469cfbaa23105d91d15ef
JAY HAN <djay.alpha@gmail.com>
2016-03-28 21:07:29 -0600

    update

--- HanHW6/HanHW6/Form1.Designer.cs
+++ HanHW6/HanHW6/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.calculateButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // calculateButton
@@ -51,6 +52,7 @@
             this.clearButton.TabIndex = 1;
             this.clearButton.Text = "Clea&r";
             this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // exitButton
             // 
@@ -60,12 +62,22 @@
             this.exitButton.TabIndex = 2;
             this.exitButton.Text = "E&xit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(83, 72);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(321, 147);
+            this.listBox.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(658, 444);
+            this.Controls.Add(this.listBox);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.calculateButton);
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox listBox;
     }
 }
 
--- HanHW6/HanHW6/Form1.cs
+++ HanHW6/HanHW6/Form1.cs
@@ -9,6 +9,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
+/*  Title: HanHW6 - Arrays and Lists
+    Requirement: You will find a file named USPopulation.txt attached above.  The file contains the midyear population of the United States, in thousands, during the           years 1950 through 1990.  The first line in the file contains the population for 1950, the second line contains the population for 1951, and so forth.
+            Create an application that reads the file's contents into an array or a List.  The application should display the following data:
+                •The average annual change in population during the time period
+                •The year with the greatest increase in population during the time period
+                •The year with the least increase in population during the time period
+    Date: 03/28/2016   
+*/
+
 namespace HanHW6
 {
     public partial class Form1 : Form
@@ -18,20 +27,55 @@ namespace HanHW6
             InitializeComponent();
         }
 
+        private void ReadNumber(List<int> numberList)
+        {
+            try
+            {
+                StreamReader inputFile;
+                string inputFilePath = Environment.CurrentDirectory;
+                inputFile = File.OpenText(Path.Combine(inputFilePath, "USPopulation.txt"));
+                while (!inputFile.EndOfStream)
+                {
+                    numberList.Add(int.Parse(inputFile.ReadLine()));
+                    listBox.Items.Add(inputFile.ReadLine());
+                }
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DisplayNumber(List<int> numberList)
+        {
+
+        }
+
         private void calculateButton_Click(object sender, EventArgs e)
         {
-            // Create an array to hold the numbers.
-            const int SIZE = 40;
-            int[] populations = new int[SIZE];
-
-            // Create streamreader object and reads the txt file.
-            StreamReader inputFile;
-            string inputFilePath = Environment.CurrentDirectory;
-            inputFile = File.OpenText(Path.Combine(inputFilePath, "USPopulation.txt"));
-            while (!inputFile.EndOfStream)
+            try
             {
-                populations = inputFile.ReadLine();
+                // Create variables and an array to hold the numbers.
+                List<int> numberList = new List<int>();
+                
+                ReadNumber(numberList);
+                
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
         }
     }
 }
