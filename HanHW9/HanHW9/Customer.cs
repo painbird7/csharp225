cd6c19c0fba7a62485ba4d215f915807a86de513
JAY HAN <djay.alpha@gmail.com>
2016-04-18 21:56:15 -0600

    asdasd

---
+++ HanHW9/HanHW9/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanHW9
+{
+    class Customer : Person
+    {
+        // Fields
+        private int _customerNumber;
+
+        // Constructor
+        public Customer()
+        {
+            _customerNumber = 0;
+        }
+
+        // CustomerNumber property
+        public int CustomerNumber
+        {
+            get { return _customerNumber; }
+            set { _customerNumber = value; }
+        }
+
+        // 
+    }
+}
